c91c1e46d9e2db2dbdaea573d7af44371fda554a
Kunal281997 <me.kaushik28@gmail.com>
2022-08-30 11:18:44 +0530

    Node Js

--- backend/config/db.js
+++
@@ -1,2 +0,0 @@
-// For connecting our express web-application to mongodb database server, we need to take help of the library named as 'mongoose'
-const mongoose = require("mongoose");
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,36 @@
 const express = require("express");
-// express is used to create web-applications.
 
-const app = express(); // 'app' is our application object representing our application.
+const app = express();
 
-// Starting the server and making it listen on a certain port..
-const port = 3000;
+// REST Architecture -- REST is a standard used by companies and developers worldwide to create API's
+// Reading..
+// Request Parameters.
+app.get("/testpath/:id", (req, res) => {
+  // Request Parameters.
+  console.log("request-parameters -- ", req.params);
 
-// creating our 1st API.
-// Creating CRUD functionallity using the methods GET, PUT, POST, DELETE.
-// And these methods are available on 'app' instance.
-
-// This get method will take two arguments.. 1st is 'path' and 2nd is 'callback' function. Also known as middleware in Express.js
-
-// The path is used by client to find this API to be invoked. // This path '/test' will be the part of the URL which client need to access this API.
-// API is nothing but a lines of code wrapped in a function which can be consumed by anyone.
-app.get("/test", (req, res) => {
-  // So, By default this callback function takes 'req' and 'res' as arguments.
-  // We can use this 'res' object to return the response back to the client.
+  console.log(req.params.id);
+  console.log("API Invoked ....");
   res.json({ success: true });
 });
 
-// Starting the server and making it listen on a certain port..
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
+// Query Parameters.
+app.get("/testing", (req, res) => {
+  console.log("request-query-parameters -- ", req.query);
+  console.log("request-query-parameters --> title -- ", req.query.title);
+  console.log("request-query-parameters --> id -- ", req.query.id);
+
+  //   console.log(req.params.id);
+  console.log("API Invoked ....");
+  res.json({ success: true });
 });
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+// const req = {
+//   params: {
+//     id: 34,
+//   },
+// };
